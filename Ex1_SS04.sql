12c89e5ab2aeeaad84cb7a3c31af41a40188b1b5
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-04-20 11:20:14 +0700

    2004

---
+++ Ex1_SS04.sql
@@ -0,0 +1,12 @@
+USE QUANLYSINHVIEN;
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+SELECT subid,subname,MAX(subject.Credit)
+FROM SUBJECT;
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select s.subid,s.subname,max(mark.mark) as "Highest Mark"
+from subject s join mark on s.subid = mark.SubId
+group by s.SubName;
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select s.studentid, s.studentname, s.address, s.phone, avg(mark.Mark) AvgMark
+from student s join mark on s.StudentId = mark.StudentId
+group by s.StudentName;
